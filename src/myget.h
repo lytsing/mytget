bb5dfde5b913fcbeb66b083deab4bf73af87fcc1
Liqing Huang <lysting@gmail.com>
2011-03-08 19:52:29 +0800

    first commit

---
+++ src/myget.h
@@ -0,0 +1,39 @@
+/*  Myget - A download accelerator for GNU/Linux
+ *  Homepage: http://myget.sf.net
+ *  Copyright (C) 2005- xiaosuo
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#ifndef _MYGET_H
+#define _MYGET_H
+
+#include "macro.h"
+#include "utils.h"
+#include "url.h"
+#include "advio.h"
+#include "tcp.h"
+#include "ftp.h"
+#include "http.h"
+#include "plugin.h"
+#include "ftpplugin.h"
+#include "httpplugin.h"
+#include "block.h"
+#include "downloader.h"
+#include "task.h"
+#include "debug.h"
+
+#endif // _MYGET_H
